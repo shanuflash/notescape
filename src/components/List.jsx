9b93a0313f5a1a6598073ed28420a58416f43a5e
Shanu S <shanuflash@gmail.com>
2023-01-17 22:57:00 +0530

    simplify

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,22 +4,28 @@ import Delete from "./Delete";
 export default function List() {
   const [result, reexecute] = useRealtime("todos");
   const { data: tasks, error, fetching } = result;
-  if (fetching) {
-    return "Loading";
-  }
-  if (!tasks || !tasks.length) {
-    return "Loading";
-  }
+  // if (fetching) {
+  //   return "Loading";
+  // }
+  // if (!tasks || !tasks.length) {
+  //   return "test";
+  // }
   return (
     <div className="list-container">
-      <div className="list">
-        {tasks.map((todo) => (
-          <div className="item-container">
-            <div className="item">{todo.text}</div>
-            <Delete id={todo.id} />
-          </div>
-        ))}
-      </div>
+      {fetching ? (
+        "Loading..."
+      ) : tasks ? (
+        <div className="list">
+          {tasks.map((todo) => (
+            <div className="item-container">
+              <div className="item">{todo.text}</div>
+              <Delete id={todo.id} />
+            </div>
+          ))}
+        </div>
+      ) : (
+        "No data found"
+      )}
     </div>
   );
 }
