729a120fdcdd674faf6fda9ea0e113ed55a37d08
Shanu S <shanuflash@gmail.com>
2023-01-29 22:04:48 +0530

    test

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import supabase from "../supabase";
 import { toast } from "react-toastify";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaTrash } from "react-icons/fa";
 
 export default function Delete({ item, textarray, setTextarray }) {
-  const handleDelete = (item) => {
+  const handleDelete = () => {
     // const { data, error } = await supabase.from("todos").delete().eq("id", id);
-    const test = textarray.filter((any) => any !== item);
-    console.log(test);
-    setTextarray(test);
+    // const test = textarray.filter((any) => any !== item);
+    // console.log(test);
+    // setTextarray(test);
+    // setTextarray(["test"]);
+    console.log(textarray);
+    setTextarray((pre) => {
+      return pre.filter((any) => any !== item);
+    });
     console.log(textarray);
     // console.log(textarray.filter((a) => a !== item));
     // console.log(textarray);
     // toast.info("Note deleted!");
   };
+
   return (
     <div>
-      <button className="item-button" onClick={() => handleDelete(item)}>
+      <button className="item-button" onClick={handleDelete}>
         <FaTrash />
       </button>
     </div>
