251b0d377f250a10355d3ae766ad2a530abf9d14
Shanu S <shanuflash@gmail.com>
2023-02-01 11:57:49 +0530

    edit function

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,11 +5,21 @@ import { useState } from "react";
 export default function List({ User, Todo, setTodo, text, setText }) {
   const [Editable, setEditable] = useState(false);
   const [dex, setdex] = useState(-1);
+  // const [Edit, setEdit] = useState(second)
   const handleDelete = (index) => {
     setTodo(Todo.filter((any, i) => i !== index));
   };
-  const handleEdit = () => {
-    // console.log(text);
+  const handleEdit = (index) => {
+    if (Editable) {
+      const nextTodo = Todo.map((c, i) => {
+        if (i === index) {
+          return text;
+        } else {
+          return c;
+        }
+      });
+      setTodo(nextTodo);
+    }
   };
 
   return (
@@ -19,27 +29,35 @@ export default function List({ User, Todo, setTodo, text, setText }) {
           <div className="list">
             {Todo.map((item, index) => (
               <div className="item-container">
-                {/* <input className="input" value={item}></input> */}
                 <div
                   contentEditable={dex === index && Editable}
+                  // onClick={(e) => setText(item)}
                   onInput={(e) => {
-                    setText((prev) => e.target.value);
-                    handleEdit();
+                    setText((prev) => e.target.innerText);
                   }}
                   key={item}
                   className="item"
+                  style={{ border: dex === index ? "1px black solid" : "" }}
                 >
                   {item}
                 </div>
                 <button
+                  // disabled={dex === index ? "false" : "true"}
                   className="item-button"
                   style={{
                     borderRadius: "0",
                     borderRight: "1px #c1e6ff solid",
                   }}
                   onClick={(e) => {
+                    setText(item);
+                    if (Editable) {
+                      handleEdit(index);
+                      setText("");
+                      setdex(-1);
+                    } else {
+                      setdex(index);
+                    }
                     Editable ? setEditable(false) : setEditable(true);
-                    setdex(index);
                   }}
                 >
                   <FaEdit />
