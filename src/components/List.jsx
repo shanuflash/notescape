e6f8de9d76f579874a3830bc54e920644a48d3a0
Shanu S <shanuflash@gmail.com>
2023-02-03 21:35:43 +0530

    handle empty items (notes, trash)

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,6 @@
 import { FaTrash, FaEdit } from "react-icons/fa";
 import { TiTick } from "react-icons/ti";
+import { FaSmileWink } from "react-icons/fa";
 import { toast } from "react-toastify";
 import { Navigate } from "react-router-dom";
 import { DataContext } from "../context/DataProvider";
@@ -31,81 +32,95 @@ export default function List({}) {
   };
 
   return (
-    <div>
-      <div className="list-container" data-aos="fade-up">
-        {User ? (
-          <div className="list">
-            {Todo.map((item, index) => (
-              <div className="item-container">
-                <div
-                  contentEditable={dex === index && Editable}
-                  onInput={(e) => {
-                    setEdit((prev) => e.target.innerText);
-                  }}
-                  // data-aos="fade-up"
-                  key={index}
-                  className="item"
-                  style={{
-                    border: dex === index ? "2px var(--highlight) solid" : "",
-                  }}
-                >
-                  {item}
-                </div>
-                <button
-                  // data-aos="fade-up"
-                  className="item-button"
-                  style={{
-                    borderRadius: "0",
-                    borderRight: "1px #001018 solid",
-                    borderLeft: "1px #001018 solid",
-                  }}
-                  onClick={(e) => {
-                    if (Edit === "" && Editable) {
-                      toast.error("Note cannot be empty!");
-                      Edit.forceUpdate();
-                      handleData();
-                    }
-                    if (Edit !== "" || Editable === false) setEdit(item);
-                    if (Edit !== "" && Editable) {
-                      if (index === dex) {
-                        handleEdit(index);
-                      } else if (index !== dex) {
-                        handleEdit(dex);
-                      }
-                      setEdit("");
-                      setEditable(true);
-                    }
-                    dex === -1 ? setdex(index) : setdex(-1);
-                    Editable ? setEditable(false) : setEditable(true);
-                  }}
-                >
-                  {Editable ? (
-                    index === dex ? (
-                      <TiTick />
-                    ) : (
-                      <FaEdit />
-                    )
-                  ) : (
-                    <FaEdit />
-                  )}
-                </button>
-                <button
-                  // data-aos="fade-up"
-                  className="item-button"
-                  onClick={() => {
-                    handleDelete(item, index);
-                    toast.error("Note deleted!");
-                  }}
-                >
-                  <FaTrash />
-                </button>
+    <>
+      {User ? (
+        <div>
+          <div className="list-container" data-aos="fade-up">
+            {Todo.length !== 0 ? (
+              <div className="list">
+                {Todo.map((item, index) => (
+                  <div className="item-container">
+                    <div
+                      contentEditable={dex === index && Editable}
+                      onInput={(e) => {
+                        setEdit((prev) => e.target.innerText);
+                      }}
+                      // data-aos="fade-up"
+                      key={index}
+                      className="item"
+                      style={{
+                        border:
+                          dex === index ? "2px var(--highlight) solid" : "",
+                      }}
+                    >
+                      {item}
+                    </div>
+                    <button
+                      // data-aos="fade-up"
+                      className="item-button"
+                      style={{
+                        borderRadius: "0",
+                        borderRight: "1px #001018 solid",
+                        borderLeft: "1px #001018 solid",
+                      }}
+                      onClick={(e) => {
+                        if (Edit === "" && Editable) {
+                          toast.error("Note cannot be empty!");
+                          Edit.forceUpdate();
+                          handleData();
+                        }
+                        if (Edit !== "" || Editable === false) setEdit(item);
+                        if (Edit !== "" && Editable) {
+                          if (index === dex) {
+                            handleEdit(index);
+                          } else if (index !== dex) {
+                            handleEdit(dex);
+                          }
+                          setEdit("");
+                          setEditable(true);
+                        }
+                        dex === -1 ? setdex(index) : setdex(-1);
+                        Editable ? setEditable(false) : setEditable(true);
+                      }}
+                    >
+                      {Editable ? (
+                        index === dex ? (
+                          <TiTick />
+                        ) : (
+                          <FaEdit />
+                        )
+                      ) : (
+                        <FaEdit />
+                      )}
+                    </button>
+                    <button
+                      // data-aos="fade-up"
+                      className="item-button"
+                      onClick={() => {
+                        handleDelete(item, index);
+                        toast.error("Note deleted!");
+                      }}
+                    >
+                      <FaTrash />
+                    </button>
+                  </div>
+                ))}
               </div>
-            ))}
+            ) : (
+              <div className="empty-trash">
+                <FaSmileWink
+                  style={{
+                    fontSize: "10vw",
+                  }}
+                />
+                Add notes to manage them!
+              </div>
+            )}
           </div>
-        ) : (
-          <Navigate replace to="/Login" />
-        )}
-      </div>
-    </div>
+        </div>
+      ) : (
+        <Navigate replace to="/Login" />
+      )}
+    </>
   );
 }
--- src/components/Trash.jsx
+++ src/components/Trash.jsx
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { DataContext } from "../context/DataProvider";
 import { toast } from "react-toastify";
 import { Navigate } from "react-router-dom";
-import { FaTrashRestore, FaTrash } from "react-icons/fa";
+import { FaTrashRestore, FaTrash, FaTrashAlt } from "react-icons/fa";
 import AOS from "aos";
 import Prompt from "./Prompt";
 import { useState } from "react";
@@ -25,67 +25,80 @@ export default function Trash() {
   return (
     <div style={{ background: "black", overflow: "hidden" }}>
       {User ? (
-        <>
-          <div className="head">
-            <div className="title head-right" data-aos="fade-right">
-              Trash (Beta)
-            </div>
-            <div
-              data-aos="fade-left"
-              className="head-left"
-              style={{
-                display: "flex",
-                alignItems: "center",
-                marginTop: "1rem",
-              }}
-            >
+        <div>
+          <div>
+            <div className="head">
+              <div className="title head-right" data-aos="fade-right">
+                Trash (Beta)
+              </div>
               <div
-                className="emptytrash user"
-                // onClick={handleEmptyTrash}
-                onClick={() => {
-                  setisOpen(!isOpen);
-                }}
+                data-aos="fade-left"
+                className="head-left"
                 style={{
-                  fontSize: "2vw",
-                  borderRadius: "2rem",
-                  // marginRight: "2rem",
-                  padding: "1rem 2rem 1rem 2rem",
+                  display: "flex",
+                  alignItems: "center",
+                  marginTop: "1rem",
                 }}
               >
-                Clear Trash <FaTrash />
+                <div
+                  className="emptytrash user"
+                  // onClick={handleEmptyTrash}
+                  onClick={() => {
+                    setisOpen(!isOpen);
+                  }}
+                  style={{
+                    fontSize: "2vw",
+                    borderRadius: "2rem",
+                    // marginRight: "2rem",
+                    padding: "1rem 2rem 1rem 2rem",
+                  }}
+                >
+                  Clear Trash <FaTrash />
+                </div>
               </div>
             </div>
-          </div>
-          {isOpen && <Prompt {...{ handleEmptyTrash, isOpen, setisOpen }} />}
+            {isOpen && <Prompt {...{ handleEmptyTrash, isOpen, setisOpen }} />}
 
-          <div
-            className="list-container list-container-misc"
-            data-aos="fade-up"
-          >
-            <div className="list">
-              {Trash.map((item, index) => (
-                <div className="item-container">
-                  <div
-                    // data-aos="fade-up"
-                    className="item"
-                  >
-                    {item}
-                  </div>
-                  <button
-                    // data-aos="fade-up"
-                    className="item-button"
-                    onClick={() => {
-                      handleRestore(item, index);
-                      toast.success("Note restored!");
-                    }}
-                  >
-                    <FaTrashRestore />
-                  </button>
+            <div
+              className="list-container list-container-misc"
+              data-aos="fade-up"
+            >
+              {Trash.length !== 0 ? (
+                <div className="list">
+                  {Trash.map((item, index) => (
+                    <div className="item-container">
+                      <div
+                        // data-aos="fade-up"
+                        className="item"
+                      >
+                        {item}
+                      </div>
+                      <button
+                        // data-aos="fade-up"
+                        className="item-button"
+                        onClick={() => {
+                          handleRestore(item, index);
+                          toast.success("Note restored!");
+                        }}
+                      >
+                        <FaTrashRestore />
+                      </button>
+                    </div>
+                  ))}
                 </div>
-              ))}
+              ) : (
+                <div className="empty-trash">
+                  <FaTrashAlt
+                    style={{
+                      fontSize: "10vw",
+                    }}
+                  />
+                  Trash is empty!
+                </div>
+              )}
             </div>
           </div>
-        </>
+        </div>
       ) : (
         <Navigate replace to="/Login" />
       )}
